7b7c5673ac1d10fed28f8e11d3d5a38e34ad7453
matezito <juan.e@genosha.com.ar>
2020-07-09 19:02:51 -0300

    fix32

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require_once(__DIR__ . '/vendor/autoload.php');
 require_once( __DIR__ . '/config.php');
+use App\Models\Database;
+new Database();
 require_once(__DIR__ .'/start.php');
 use App\Controllers\Guia;?>
 <?php require './tpl/head.php' ?>
--- start.php
+++ start.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Models\Database;
 
-new Database();
 
 use App\Controllers\Guia;
 Guia::eliminar_fecha(date('Y-m-d'));
